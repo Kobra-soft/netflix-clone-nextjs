4d65c5ff662e59af64a8ce363533bdc6d493678c
user1 <code.softwaredevg@gmail.com>
2023-04-10 16:50:27 +0100

    5

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    namespace globalThis {
+        var prismadb: PrismaClient
+    }
+}
+
+export default client;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
-import Input from "@/components/Input";
+import axios from 'axios';
 import { useCallback, useState } from "react";
+import Input from "@/components/Input";
 
 const Auth = () => {
     const [email, setEmail] = useState('');
@@ -11,6 +12,18 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, [])
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -48,7 +61,7 @@ const Auth = () => {
                                 value={password} 
                                 />  
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
