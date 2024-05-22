792c97eb2b6872333ed10ac65ff7bd9075ef0704
kobra-soft <code.softwaredev@gmail.com>
2024-05-22 16:15:10 +0100

    Commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
 import { useCallback, useEffect, useState } from "react";
-import React from 'react';
+import React from "react";
 
-import { AiFillCaretDown } from "react-icons/ai"
+import { AiFillCaretDown } from "react-icons/ai";
 
-import NetflixSearchLogo from "./NetflixSearchLogo"
+import NetflixSearchLogo from "./NetflixSearchLogo";
 import NetflixBellLogo from "./NetflixBellLogo";
 
 import NavbarItem from "./NavbarItem";
@@ -14,38 +14,38 @@ import AccountMenu from "./AccountMenu";
 const TOP_OFFSET = 66;
 
 const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [ShowBackground, setShowBackground] = useState(false);
 
-    const [showMobileMenu, setShowMobileMenu] = useState(false);
-    const [showAccountMenu, setShowAccountMenu] = useState(false);
-    const [ShowBackground, setShowBackground] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > TOP_OFFSET) {
-                setShowBackground(true);
-            } else {
-                setShowBackground(false);
-            }
-        }
-        window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        }
-    }, []);
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
 
-    const toggleMobileMenu = useCallback(() => {
-        setShowMobileMenu((current) => !current);
-    }, []);
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
 
-    const toggleAccountMenu = useCallback(() => {
-        setShowAccountMenu((current) => !current);
-    }, []);
-
-    return (
-        <nav className="bg-black w-full fixed z-40">
-            {/* change all px values to pl padding left, as could not move the flex right (navbar items!!) */}
-            <div className={`
+  return (
+    <nav className="bg-black w-full fixed z-40">
+      {/* change all px values to pl padding left, as could not move the flex right (navbar items!!) */}
+      <div
+        className={`
             px-[1.04rem]
             sm:pl-[1.6rem]
             md:pl-[2.26rem]
@@ -59,13 +59,18 @@ const Navbar = () => {
             xl:py-[18px]
 
             flex flex-row items-center transition duration-500
-            ${ShowBackground ? 'bg-[#0b0b0b] bg-opacity-90' : ''}
+            ${ShowBackground ? "bg-[#0b0b0b] bg-opacity-90" : ""}
             `}
-            >
-                <img className="h-[0.92rem] sm:h-[1.0rem] md:h-[1.0rem] lg:h-[1.55rem]" src="/images/logo.png" alt="" />
-                {/* <img className="h-[0.92rem] sm:h-[1.0rem] md:h-[1.0rem] lg:h-[1.55rem]" src="/images/Jackflix2.png" alt="" /> */}
+      >
+        <img
+          className="h-[0.92rem] sm:h-[1.0rem] md:h-[1.0rem] lg:h-[1.55rem]"
+          src="/images/logo.png"
+          alt=""
+        />
+        {/* <img className="h-[0.92rem] sm:h-[1.0rem] md:h-[1.0rem] lg:h-[1.55rem]" src="/images/Jackflix2.png" alt="" /> */}
 
-                <div className="
+        <div
+          className="
                 flex-row
 
                 ml-[2.88rem] sm:ml-[1.40rem] md:ml-[1.40rem] xl:ml-[2.88rem]
@@ -73,43 +78,61 @@ const Navbar = () => {
                 hidden
                 md:flex lg:flex
                 "
-                >
-                    <NavbarItemHome label="Home"/>
-                    <NavbarItem label="Series" />
-                    <NavbarItem label="Films" />
-                    <NavbarItem label="New & Popular" />
-                    <NavbarItem label="My List" />
-                    <NavbarItem label="Browse by Languages" />
-                </div>
-                <div onClick={toggleMobileMenu}
-                className="lg:hidden md:hidden flex flex-row items-center gap-1 ml-6 cursor-pointer relative">
-                <p className="text-white 
+        >
+          <NavbarItemHome label="Home" />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Films" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by Languages" />
+        </div>
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden md:hidden flex flex-row items-center gap-1 ml-6 cursor-pointer relative"
+        >
+          <p
+            className="text-white 
                 text-[8.0px] md:text-[12.6px] lg:text-[13.3px] 
-                tracking-[.044em] items-center">Browse</p>
-                    <AiFillCaretDown className={`text-white ml-0 w-[0.82rem] h-4 transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
-                    <MobileMenu visible={showMobileMenu}/>
-                </div>
+                tracking-[.044em] items-center"
+          >
+            Browse
+          </p>
+          <AiFillCaretDown
+            className={`text-white ml-0 w-[0.82rem] h-4 transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
 
-                <div className="flex flex-1 justify-end space-x-0 items-center
-                 gap-4 sm:gap-4 md:gap-4 lg:gap-5 xl:gap-5">
-                    <div className="hover:text-gray-300 cursor-pointer transition">
-                    <NetflixSearchLogo/>
-                    </div>
-                    <div className="hover:text-gray-300 cursor-pointer transition">
-                    <NetflixBellLogo/>
-                    </div>
-                    <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
-                    <div className="w-7 h-7 md:w-[1.96rem] md:h-[1.96rem] lg:w-[1.96rem] lg:h-[2rem] rounded-[0.24rem] overflow-hidden">
-                        <img src="/images/default-squidgame2.png"></img>
-                    </div>
-                    <AiFillCaretDown className={`text-white w-[0.82rem] lg:mr-[3.50rem] transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`}/>
-                    <AccountMenu visible={showAccountMenu}/>
-                    </div>
-                </div>
+        <div
+          className="flex flex-1 justify-end space-x-0 items-center
+                 gap-4 sm:gap-4 md:gap-4 lg:gap-5 xl:gap-5"
+        >
+          <div className="hover:text-gray-300 cursor-pointer transition">
+            <NetflixSearchLogo />
+          </div>
+          <div className="hover:text-gray-300 cursor-pointer transition">
+            <NetflixBellLogo />
+          </div>
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-7 h-7 md:w-[1.96rem] md:h-[1.96rem] lg:w-[1.96rem] lg:h-[2rem] rounded-[0.24rem] overflow-hidden">
+              <img src="/images/default-squidgame2.png"></img>
             </div>
-        </nav>
-    )
-}
+            <AiFillCaretDown
+              className={`text-white w-[0.82rem] lg:mr-[3.50rem] transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
-
--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 interface NavbarItemProps {
-    label: string;
+  label: string;
 }
 
-const NavbarItem:  React.FC<NavbarItemProps> = ({
-    label
-}) => {
-    return (
-        <div className="text-[#cecdcd] 
+const NavbarItem: React.FC<NavbarItemProps> = ({ label }) => {
+  return (
+    <div
+      className="text-[#cecdcd] 
         md:tracking-[.044em] lg:tracking-[.040em] xl:tracking-[.031em] 
         md:text-[8px] lg:text-[11.2px] xl:text-[13.3px] 
-        cursor-pointer hover:text-[#a1a0a0] transition-colors">
-            {label}
-        </div>
-    )
-}
+        cursor-pointer hover:text-[#a1a0a0] transition-colors"
+    >
+      {label}
+    </div>
+  );
+};
 
 export default NavbarItem;
